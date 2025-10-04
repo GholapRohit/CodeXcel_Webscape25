5ef00f411b233c3f60498e92decc26c412849a17
GholapRohit <rohitgholap2005@gmail.com>
2025-10-04 11:20:09 +0530

    search

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let mealSearch = document.getElementById("meal-search");
 let searchBtn = document.getElementById("search-btn");
 
+let databox = document.getElementById("data");
+
 searchBtn.addEventListener("click", () => {
   let input = mealSearch.value;
   search(input);
@@ -12,4 +14,13 @@ let search = async (input) => {
   );
   let data = await res.json();
   console.log(data);
+  console.log(data.meals[0]);
+  console.log(data.meals[0].strArea);
+  let ul = document.createElement("ul");
+  for (key in data.meals[0]) {
+    if (key.includes("strIngredient") && data.meals[0][key] != "") {
+      console.log(data.meals[0][key]);
+      let li = document.createElement;
+    }
+  }
 };
